96320d44d37f68de0e0f558d2557a534632265bf
jia237377144 <237377144@qq.com>
2016-10-26 14:53:28 +0800

    create swagger

---
+++ webSwagger/App_Start/WebApiMOdule.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webSwagger.App_Start
+{
+    public class WebApiMOdule
+    {
+    }
+}
---
+++ webSwagger/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="webSwagger.WebApiApplication" Language="C#" %>
